c4d6d67c0f8582baaca82d77a2b7f8a18dc05735
jayaprem <36623938+jayaprem@users.noreply.github.com>
2019-06-06 15:43:56 +0530

    Create isProbablePrime() .java

---
+++ isProbablePrime() .java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        BigInteger b=sc.nextBigInteger();
+        sc.close();
+        System.out.println(b. isProbablePrime(1)?"prime":"not prime");
+        
+    }
+}
+INPUT:
+13
+OUTPUT:
+prime
